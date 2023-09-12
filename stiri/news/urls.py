9a2310ea2d67340d85c7ba3c84506fd0f44aebdd
Neonhizer <rusu.victor002@gmail.com>
2023-09-12 22:01:31 +0300

    actualizare, news/scrape

--- stiri/news/urls.py
+++ stiri/news/urls.py
@@ -2,8 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('', views.home, name='home'),
-    path('scrape_techcrunch/', views.scrape_techcrunch, name='scrape_techcrunch'),
-    path('get_news/', views.get_news, name='get_news'),
+    path('scrape/', views.scrape_techcrunch, name='scrape'),
+   # path('news/scrape/', views.scrape_news, name='scrape_news'),
+    
 ]
+
+
+
+
+    # path('get_news/', views.get_news, name='get_news'),
+    # path('scrape/', views.scrape_techcrunch, name='scrape'),  
+    # path('search/', views.search_results, name='search_results'),
+    # path('search/category/', views.search_results_category, name='search_results_category'),
+    # path('search/location/', views.search_results_location, name='search_results_location'),
+    # path('search/topic/', views.search_results_topic, name='search_results_topic'),
+    # path('search/author/', views.search_results_author, name='search_results_author'),
+    # path('search/date/', views.search_results_date, name='search_results_date'),
+
--- stiri/news/views.py
+++ stiri/news/views.py
@@ -2,23 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 from django.shortcuts import render
 from pymongo import MongoClient
-from .models import Article  
+from .models import Article
 from django.http import JsonResponse
 from .models import Article
+# from django.http import HttpResponse
+# from django.conf.urls import url
 
 
 def scrape_techcrunch(request):
     url = "https://techcrunch.com/"
-
     response = requests.get(url)
-
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         articles = soup.find_all('article')
         article_data = []
 
         for article in articles:
-            title = article.find('h2').get_text()
+            title = article.find('h3').get_text()
             link = article.find('a')['href']
             image_tag = article.find('img')
             image = image_tag['src'] if image_tag else ''
@@ -31,16 +31,18 @@ def scrape_techcrunch(request):
                 'image': image,
                 'author': author,
             })
-
+        context = {
+            'articles': article_data,
+        }
         # Save the articles in the MongoDB database
-        client = MongoClient('localhost', 27017) 
-        db = client['news'] 
-        news_collection = db['techcrunch_news']  
+        client = MongoClient('localhost', 27017)
+        db = client['news']
+        news_collection = db['techcrunch_news']
 
         for data in article_data:
             news_collection.insert_one(data)
 
-    return render(request, 'news/scrape_techcrunch.html')
+    return render(request, 'news/scrape_techcrunch.html', context)
 
 def home(request):
     # Fetch all the articles from the database
@@ -58,7 +60,7 @@ def get_news(request):
     # Extract all the news from the database
     news = Article.objects.all()
 
-   
+
     news_data = {
         'titles': [Article.title for stire in news],
         'links': [Article.url for stire in news],
@@ -66,3 +68,17 @@ def get_news(request):
 
     # Return the data as JSON
     return JsonResponse(news_data)
+
+
+
+
+# def search_results(request):
+#     if'search_term' in request.GET and request.GET["search_term"]:
+#         search_term = request.GET.get("search_term")
+#         searched_articles = Article.search_by_title(search_term)
+#         message = f"{search_term}"
+
+
+# def index(request):
+#     return HttpResponse('Hello, world!')
+
